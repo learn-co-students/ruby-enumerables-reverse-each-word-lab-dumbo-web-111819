aeb0298308a808798fedde97a48e2e2071f96050
Annie Zheng <anniezheng.anniee@gmail.com>
2019-11-14 02:30:47 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,4 @@
 def reverse_each_word(string)
-  new = string.split(" ")
-  arr = []
-  new.each do |word|
-    arr << word.reverse 
-  end 
-  arr.join(" ")
-endv
+  string.split.collect {|word| word.reverse}.join(" ")
+end 
+  
